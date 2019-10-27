59fa73087bea07a36a06c1b5164a9ac5753a6109
Peter <piotr.szaran@gmail.com>
2019-10-27 16:53:00 +0100

    v21 - ErrorRepositoryImpl: implemented .findAll() method
    ErrorService added 2 methods: getMapOfErrors() and showErrors()

--- src/main/java/pl/szaran/repository/ErrorRepositoryImpl.java
+++ src/main/java/pl/szaran/repository/ErrorRepositoryImpl.java
@@ -4,6 +4,8 @@ import pl.szaran.model.Error;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class ErrorRepositoryImpl extends AbstractGenericRepository<Error> implements ErrorRepository {
@@ -33,5 +35,31 @@ public class ErrorRepositoryImpl extends AbstractGenericRepository<Error> implem
                 entityManager.close();
             }
         }
+    }
+
+    @Override
+    public List<Error> findAll() {
+        List<Error> errorsFromDB = new ArrayList<>();
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
+
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx= entityManager.getTransaction();
+            tx.begin();
+
+            errorsFromDB = entityManager.createQuery("select e from Error e ORDER BY e.date", Error.class).getResultList();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return errorsFromDB;
     }
 }
--- src/main/java/pl/szaran/service/ErrorService.java
+++ src/main/java/pl/szaran/service/ErrorService.java
@@ -6,6 +6,10 @@ import pl.szaran.repository.ErrorRepository;
 import pl.szaran.repository.ErrorRepositoryImpl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ErrorService implements ModelMapper{
     final ErrorRepository errorRepository = new ErrorRepositoryImpl();
@@ -15,8 +19,24 @@ public class ErrorService implements ModelMapper{
         Error error = null;
         error = ModelMapper.fromErrorDTOToError(ErrorDTO.builder().message(message).date(LocalDateTime.now()).build());
         errorRepository.saveOrUpdate(error);
-
-
     }
 
+    public Map<Integer, Error> getMapOfErrors() {
+        List<Error> list = new ArrayList<>(errorRepository.findAll());
+        int i = 1;
+        Map<Integer, Error> errorsMap = new HashMap<>();
+        for (Error e: list
+             ) {
+            errorsMap.put(i, e);
+            i++;
+        }
+        return errorsMap;
+    }
+
+    public void showErrors() {
+        Map<Integer, Error> map = getMapOfErrors();
+        for (Map.Entry<Integer, Error> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ". " + entry.getValue().getDate() + ", " + entry.getValue().getMessage());
+        }
+    }
 }
