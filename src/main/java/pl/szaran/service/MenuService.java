e749eee80033c946cabb5d5792124b811633c5c6
Piotr Szaran <piotr.szaran@gmail.com>
2019-11-06 16:14:41 +0100

    v39 MenuService: insertOrder() added.

--- src/main/java/pl/szaran/service/MenuService.java
+++ src/main/java/pl/szaran/service/MenuService.java
@@ -1,8 +1,16 @@
 package pl.szaran.service;
 
 import pl.szaran.dto.*;
+import pl.szaran.exceptions.ExceptionCode;
 import pl.szaran.exceptions.MyException;
+import pl.szaran.model.Customer;
+import pl.szaran.model.Payment;
+import pl.szaran.model.Product;
+import pl.szaran.model.Stock;
+
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
 
 public final class MenuService {
 
@@ -283,6 +291,105 @@ public final class MenuService {
     }
 
     private void insertOrder() {
+        int choice;
+        BigDecimal discount;
+
+        Product product = null;
+        Customer customer = null;
+        Payment payment = null;
+        Stock stock = null;
+        int quantity;
+
+        /**
+         * WYBÓR PRODUKTU
+         * generuję mapę produktów według wybranej kategorii metodą showProductsByCategory()
+         * (którą wykorzystuję też w menu do podglądu produktów)
+         * użytkownik wybiera produkt z mapy wyświetlonej na ekranie w formie listy
+         */
+
+        Map<Integer, Product> productMap = productService.showProductsByCategory();
+        choice = UserDataService.getInt("Wybierz produkt:");
+        if (choice < 1 && choice > productMap.size()) {
+            throw new MyException(ExceptionCode.SERVICE, "PRODUCT - INVALID CHOICE");
+        } else {
+            product = productMap.get(choice);
+            System.out.println("Wybrano " + product.getName() + ", którego cena wynosi " + product.getPrice());
+        }
+
+        /**
+         * WYBÓR KLIENTA
+         * generuję mapę klientów metodą showCustomers(), którą wykorzystuję też w menu do podglądu produktów.
+         * użytkownik wybiera klienta z mapy wyświetlonej na ekranie w formie listy
+         */
+
+        Map<Integer, Customer> customerMap = customerService.showMapOfCustomers();
+        choice = UserDataService.getInt("Wybierz klienta:");
+        if (choice < 1 && choice > customerMap.size()) {
+            throw new MyException(ExceptionCode.SERVICE, "CUSTOMER - INVALID CHOICE");
+        } else {
+            customer = customerMap.get(choice);
+            System.out.println("Wybrany klient: " + customer.getName() + " " + customer.getSurname() + ", lat " + customer.getAge() + " " + customer.getCountry().getName());
+        }
+
+        /**
+         * WYBÓR PŁATNOSCI
+         * generuję mapę płatności metodą showPayments(), którą wykorzystję też w menu do podglądu płatności.
+         * użytkownik wybiera płatność z mapy, wyświetlonej na ekranie w formie listy
+         */
+
+        Map<Integer, Payment> paymentMap = paymentService.showPayments();
+        choice = UserDataService.getInt("Wynierz płatność:");
+        if (choice < 1 && choice > paymentMap.size()) {
+            throw new MyException(ExceptionCode.SERVICE, "PAYMENT - INVALID CHOICE");
+        } else {
+            payment = paymentMap.get(choice);
+            System.out.println("Wybrano płatoność: " + payment.getPayment());
+        }
+
+        /**
+         * WYBÓR SKLEPU Z MAGAZYNU w różnych sklepach może pojawić się ten sam towar w różenj ilości
+         */
+
+        Map<Integer, Stock> stockMap = stockService.getMapOfStockProducts(product.getName());
+        System.out.println("Wybrany produkt: " + product.getName());
+        for (Map.Entry<Integer, Stock> entry : stockMap.entrySet()) {
+            System.out.println(entry.getKey() + ". " + " Sklep: " + entry.getValue().getShop().getName()
+                    + " ilość w magazynie: " + entry.getValue().getQuantity());
+        }
+
+        System.out.println();
+        choice = UserDataService.getInt("Wybierz sklep:");
+        if (choice < 1 && choice > stockMap.size()) {
+            throw new MyException(ExceptionCode.SERVICE, "STOCK - INVALID CHOICE");
+        } else {
+            stock = stockMap.get(choice);
+        }
+        System.out.println("Wybrano sklep: " + stock.getShop().getName());
+        System.out.println("Dostępna ilość sztuk towaru: " + stock.getQuantity());
+
+        choice = UserDataService.getInt("Podaj ilość zamawianego towaru");
+        if (choice < 1 && choice > stock.getQuantity()) {
+            errorService.addError("ORDER;" + "SELECTED QUANTITY IS INCORRECT");
+            throw new MyException(ExceptionCode.SERVICE, "STOCK - INVALID QUANTITY");
+        } else {
+            quantity = choice;
+        }
+
+        System.out.println();
+        discount = UserDataService.getBigDecimal("Wprowadź zniżkę (0-1):");
+        //walidacja jest w OrderValidator
+
+        OrderDTO orderDTO = OrderDTO.builder()
+                .customerDTO(ModelMapper.fromCustomerToCustomerDTO(customer))
+                .productDTO(ModelMapper.fromProductToProductDTO(product))
+                .paymentDTO(ModelMapper.fromPaymentToPaymentDTO(payment))
+                .date(LocalDate.now())
+                .quantity(quantity)
+                .discount(discount)
+                .build();
+
+        orderService.addOrder(orderDTO);
+        stockService.updateStockQuantity(stock, quantity); //pomniejszenie stanu magazynu o zamawianą ilość towaru
     }
 
     private void insertProducer() {
