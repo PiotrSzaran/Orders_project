a318bf605c765be27029bb9ea1d055b7ecfc89a2
Piotr Szaran <piotr.szaran@gmail.com>
2019-11-07 17:09:00 +0100

    v47 ErrorService: addError(ErrorDTO errorDTO) added. This can be used to import data from json to DB

--- src/main/java/pl/szaran/service/ErrorService.java
+++ src/main/java/pl/szaran/service/ErrorService.java
@@ -1,6 +1,8 @@
 package pl.szaran.service;
 
 import pl.szaran.dto.ErrorDTO;
+import pl.szaran.exceptions.ExceptionCode;
+import pl.szaran.exceptions.MyException;
 import pl.szaran.model.Error;
 import pl.szaran.repository.ErrorRepository;
 import pl.szaran.repository.ErrorRepositoryImpl;
@@ -12,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ErrorService implements ModelMapper{
+public class ErrorService implements ModelMapper {
     final ErrorRepository errorRepository = new ErrorRepositoryImpl();
 
     public void addError(String message) {
@@ -26,12 +28,36 @@ public class ErrorService implements ModelMapper{
         List<Error> list = new ArrayList<>(errorRepository.findAll());
         int i = 1;
         Map<Integer, Error> errorsMap = new HashMap<>();
-        for (Error e: list
-             ) {
+        for (Error e : list
+        ) {
             errorsMap.put(i, e);
             i++;
         }
         return errorsMap;
+    }
+
+    //@overload
+    //ta metoda służy jedynie do importu z json do DB
+    public void addError(ErrorDTO errorDTO) {
+        //todo rozważyć walidację (czy warto sprawdzać dane z pliku json, skoro został on wygenerowany na podstawie danych z bazy? Moim zdaniem nie ma sensu
+        Error error = null;
+
+        if (errorDTO.getId() != null) {
+            error = errorRepository.findById(errorDTO.getId()).orElse(null);
+        }
+
+        if (error == null) {
+            error = ModelMapper.fromErrorDTOToError(errorDTO);
+            errorRepository.saveOrUpdate(error);
+        }else {
+            String message = "ERROR " + errorDTO.getDate() + " " + errorDTO.getMessage() + " ALREADY ADDED";
+            System.out.println(message);
+        }
+        if (error == null) {
+            System.out.println("PROBLEMS WITH ERROR");
+            throw new MyException(ExceptionCode.SERVICE, "PROBLEMS WITH ERROR");
+        }
+
     }
 
     public void showErrors() {
@@ -49,4 +75,9 @@ public class ErrorService implements ModelMapper{
                 .map(errorDTO -> ErrorDTO.builder().date(errorDTO.getDate()).message(errorDTO.getMessage()).build())
                 .collect(Collectors.toList());
     }
+
+    public void deleteErrors() {
+        errorRepository.deleteAll();
+    }
+
 }
